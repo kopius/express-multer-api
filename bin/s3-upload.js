af584a24ba313a81ec8898a6a2abb98d96999df4
kopiusmaximus <alexander.kopynec@gmail.com>
2016-10-13 15:41:04 -0400

    Prepare AWS S3 options
    Define a function that builds a dictionary of options for
    the file upload to AWS, including: bucket name, file buffer,
    an ACL (public), content type, and key.

--- bin/s3-upload.js
+++ bin/s3-upload.js
@@ -35,12 +35,33 @@ const parseFile = (fileBuffer) => {
   return file;
 };
 
-const logMessage = (file) => {
-  console.log(`${filename} is ${file.data.length} bytes long and is of mime type ${file.mime}`);
+const upload = (file) => {
+  const options = {
+    // get the bucket name from your AWS S3 console
+    Bucket: 'kopius-wdi',
+    // attach the fileBuffer as a stream to send to S3
+    Body: file.data,
+    // allow anyone to access the URL of the uploaded file
+    ACL: 'public-read',
+    // tell S3 what the mime-type is
+    ContentType: file.mime,
+    // pick a filename for S3 to use
+    Key: `test/test.${file.ext}`
+  };
+  // don't actually upload data yet; just pass the data down the Promise chain
+  return Promise.resolve(options);
 };
 
+const logMessage = (upload) => {
+  delete upload.Body; // this is temporary so we can log the rest of the options
+  // turn the pojo into a string so we can see it on the console
+  console.log(`the upload options are ${JSON.stringify(upload)}`);
+};
+
+// call readFile and pass it the filename to initiate the Promise chain
 readFile(filename)
 .then(parseFile)
+.then(upload)
 .then(logMessage)
 .catch(console.error)
 ;
