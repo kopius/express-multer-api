fb322de771e05498a362764e0d26d305deb8a4b8
kopiusmaximus <alexander.kopynec@gmail.com>
2016-10-13 11:20:09 -0400

    Change project name globally
    Change project name globally to express-multer-api

--- app/controllers/root.js
+++ app/controllers/root.js
@@ -6,7 +6,7 @@ const controller = require('lib/wiring/controller');
 const root = (req, res) => {
   res.json({
     index: {
-      title: 'Express Template',
+      title: 'express-multer-api',
       environment: req.app.get('env'),
     },
   });
