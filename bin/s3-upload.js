fb354e0980cd39d7525d0dbc940d9795014ae633
kopiusmaximus <alexander.kopynec@gmail.com>
2016-10-14 11:47:06 -0400

    Name file uploads using crypto module
    - Define wrapper functions for naming the file
    - add exlanatory comments to functions

--- bin/s3-upload.js
+++ bin/s3-upload.js
@@ -11,13 +11,17 @@ require('dotenv').config();
 // to an intermediary variable'
 
 const fs = require('fs');
+const crypto = require('crypto');
+
 const fileType = require('file-type');
 // we are breaking convention to name this in all caps so that it is
 // consistent with the Amazon documentation
 const AWS = require('aws-sdk');
 
+// get the file path argument from the command line
 const filename = process.argv[2] || '';
 
+//
 const readFile = (filename) => {
   return new Promise((resolve, reject) => {
     fs.readFile(filename, (error, data) => {
@@ -30,25 +34,53 @@ const readFile = (filename) => {
   });
 };
 
-// return a default object in the case that file-type is given an unsupported
-// file type to read
+// return a default object in the case that fileType is given an unsupported
+// filetype to read
 const mimeType = (data) => {
   return Object.assign({
-    // these default properties say 'hey, this is just a stream of bytes
-    // with no metadata'
+    // these default properties say 'hey, this
+    // is just a stream of bytes with no metadata'
     ext: 'bin',
-    mime: 'application/octet-stream'
+    mime: 'application/octet-stream',
   }, fileType(data));
 };
 
-// attach the data and the mime type to a returned object
+// parse the file buffer and pin it to a file object
 const parseFile = (fileBuffer) => {
   let file = mimeType(fileBuffer);
   file.data = fileBuffer;
   return file;
 };
 
-// returns and instance of the S3 manager that will be authenticated
+// generate a random hex string for unique file naming
+const randomHexString = (length) => {
+  return new Promise((resolve, reject) => {
+    crypto.randomBytes(length, (error, buffer) => {
+      if (error) {
+        reject(error);
+      }
+
+      resolve(buffer.toString('hex'));
+    });
+  });
+};
+
+// name the file for the upload
+const nameFile = (file) => {
+  return randomHexString(16)
+  .then((val) => {
+    file.name = val;
+    return file;
+  });
+};
+
+// name the directory
+const nameDirectory = (file) => {
+  file.dir = new Date().toISOString().split('T')[0];
+  return file;
+};
+
+// returns an instance of the S3 manager that will be authenticated
 const s3 = new AWS.S3({
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY,
@@ -56,6 +88,8 @@ const s3 = new AWS.S3({
   }
 });
 
+// define options for the file upload and return a new Promise
+// with a callback that attempts to upload the file
 const upload = (file) => {
   const options = {
     // get the bucket name from your AWS S3 console
@@ -66,12 +100,12 @@ const upload = (file) => {
     ACL: 'public-read',
     // tell S3 what the mime-type is
     ContentType: file.mime,
-    // pick a filename for S3 to use
-    Key: `test/test.${file.ext}`
+    // pick a filename for S3 to use for the upload
+    Key: `${file.dir}/${file.name}.${file.ext}`
   };
 
   return new Promise((resolve, reject) => {
-    s3.upload(options, (error,data) => {
+    s3.upload(options, (error, data) => {
       if (error) {
         reject(error);
       }
@@ -81,6 +115,7 @@ const upload = (file) => {
   });
 };
 
+// log a message about the response we receive back from AWS S3
 const logMessage = (response) => {
   // turn the pojo into a string so we can see it on the console
   console.log(`the response from AWS was ${JSON.stringify(response)}`);
@@ -89,6 +124,8 @@ const logMessage = (response) => {
 // call readFile and pass it the filename to initiate the Promise chain
 readFile(filename)
 .then(parseFile)
+.then(nameFile)
+.then(nameDirectory)
 .then(upload)
 .then(logMessage)
 .catch(console.error)
