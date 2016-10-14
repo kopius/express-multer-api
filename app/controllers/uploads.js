0dc11711f479f8beb3efa4540e377384c3edd8d9
kopiusmaximus <alexander.kopynec@gmail.com>
2016-10-14 15:24:48 -0400

    Complete file upload on server
    - use upload library to initiate upload to Amazon
    - store response info from AWS in Mongo
    - send JSON back to the client

--- app/controllers/uploads.js
+++ app/controllers/uploads.js
@@ -7,6 +7,8 @@ const multer = require('app/middleware').multer;
 const models = require('app/models');
 const Upload = models.upload;
 
+const upload = require('lib/s3-upload').upload;
+
 // const authenticate = require('./concerns/authenticate');
 
 const index = (req, res, next) => {
@@ -22,51 +24,18 @@ const show = (req, res, next) => {
 };
 
 const create = (req, res, next) => {
-  // let upload = Object.assign(req.body.upload, {
-  //   _owner: req.currentUser._id,
-  // });
-  let upload = {
-    file: req.file,
-    comment: req.body.upload.comment,
-  };
-
-  res.json({ upload });
-  // Upload.create(upload)
-  //   .then(upload => res.json({ upload }))
-  //   .catch(err => next(err));
+  upload(req.file.buffer) // use a stream instead of req.file
+  .then((response) => {
+    return {
+      location: response.Location, // from S3
+      comment: req.body.upload.comment, // from our client
+    };
+  })
+  .then(Upload.create)
+  .then(upload => res.json({ upload }))
+  .catch(error => next(error))
+  ;
 };
-
-// what does it mean to update an upload? change tags, comments, description
-// we're not going to worry about this for now
-// const update = (req, res, next) => {
-//   let search = { _id: req.params.id, _owner: req.currentUser._id };
-//   Upload.findOne(search)
-//     .then(upload => {
-//       if (!upload) {
-//         return next();
-//       }
-//
-//       delete req.body._owner;  // disallow owner reassignment.
-//       return upload.update(req.body.upload)
-//         .then(() => res.sendStatus(200));
-//     })
-//     .catch(err => next(err));
-// };
-
-// not worrying about destroy for now
-// const destroy = (req, res, next) => {
-//   let search = { _id: req.params.id, _owner: req.currentUser._id };
-//   Upload.findOne(search)
-//     .then(upload => {
-//       if (!upload) {
-//         return next();
-//       }
-//
-//       return upload.remove()
-//         .then(() => res.sendStatus(200));
-//     })
-//     .catch(err => next(err));
-// };
 
 module.exports = controller({
   index,
--- config/routes.js
+++ config/routes.js
@@ -18,7 +18,7 @@ module.exports = require('lib/wiring/routes')
 .resources('users', { only: ['index', 'show'] })
 
 // custom routes
-.post('/uploads', 'uploads#create')
+.resources('uploads', { only: ['index', 'show', 'create'] })
 
 // all routes created
 ;
